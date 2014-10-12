27d2fe4fba3a109060ac25700d0f8411fdb9772d
Denise <yu.denise.d@gmail.com>
2014-10-12 03:13:20 +0100

    added remote database location

--- app/datamapper_helper.rb
+++ app/datamapper_helper.rb
@@ -1,6 +1,6 @@
 env = ENV["RACK_ENV"] || "development"
 
-DataMapper.setup(:default, "postgres://localhost/bookmark_manager_#{env}")
+DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres://localhost/confession_board_#{env}")
 
 DataMapper.finalize 
 
