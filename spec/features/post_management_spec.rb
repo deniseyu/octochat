483b3fb0aa5f4d74a5e47bd83dd9bac6469a591f
Denise <yu.denise.d@gmail.com>
2014-10-12 01:33:59 +0100

    Added protection against form injections

--- spec/features/post_management_spec.rb
+++ spec/features/post_management_spec.rb
@@ -21,5 +21,13 @@ feature 'Users who are logged in' do
     expect(page).to have_content "Fish are friends, not food!"
     expect(page).to have_content "by Nemo"
   end
+
+  scenario 'should not be able to execute HTML via posts' do 
+    sign_in 
+    new_post("<img src='http://i.imgur.com/yOxuiQ2.jpg'>")
+    expect(page).to have_content "<img src='http://i.imgur.com/yOxuiQ2.jpg'>"
+  end
+
+
   
 end
